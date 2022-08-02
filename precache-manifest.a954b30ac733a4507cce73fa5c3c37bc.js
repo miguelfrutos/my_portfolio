850c5bccfe68ccb6a0dc0b5f46e544758ba73cd4
miguelfrutos <miguel.frutos@hotmail.com>
2022-08-02 16:56:31 +0200

    Updates

---
+++ precache-manifest.a954b30ac733a4507cce73fa5c3c37bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afee5e7126a36b5a10407ceaa56d1b63",
+    "url": "/miguelfrutos/portfolio/index.html"
+  },
+  {
+    "revision": "5963d2a100993c4c0305",
+    "url": "/miguelfrutos/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5b207248b283bc042552",
+    "url": "/miguelfrutos/portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "5963d2a100993c4c0305",
+    "url": "/miguelfrutos/portfolio/static/js/2.5a4fa55a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/miguelfrutos/portfolio/static/js/2.5a4fa55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b207248b283bc042552",
+    "url": "/miguelfrutos/portfolio/static/js/main.ef34f2cf.chunk.js"
+  },
+  {
+    "revision": "1f2421b82547a287a80c",
+    "url": "/miguelfrutos/portfolio/static/js/runtime-main.5b39b314.js"
+  }
+]);
