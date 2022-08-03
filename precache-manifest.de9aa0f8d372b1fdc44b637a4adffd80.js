d974cfd208186074a53be8798818d9cf6e6da2d3
miguelfrutos <miguel.frutos@hotmail.com>
2022-08-03 18:27:52 +0200

    Updates

---
+++ precache-manifest.de9aa0f8d372b1fdc44b637a4adffd80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad62615dcdf885ac0c4a259365af96b6",
+    "url": "/miguelfrutos.github.io/ultimate_portfolio2/index.html"
+  },
+  {
+    "revision": "4f79204cd44166e2a522",
+    "url": "/miguelfrutos.github.io/ultimate_portfolio2/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "cf1d08881211d5de5c3c",
+    "url": "/miguelfrutos.github.io/ultimate_portfolio2/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "4f79204cd44166e2a522",
+    "url": "/miguelfrutos.github.io/ultimate_portfolio2/static/js/2.71d934f0.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/miguelfrutos.github.io/ultimate_portfolio2/static/js/2.71d934f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1d08881211d5de5c3c",
+    "url": "/miguelfrutos.github.io/ultimate_portfolio2/static/js/main.89cdf562.chunk.js"
+  },
+  {
+    "revision": "fd9ab8dcd9c37a8ca51a",
+    "url": "/miguelfrutos.github.io/ultimate_portfolio2/static/js/runtime-main.c2b56d1a.js"
+  }
+]);
